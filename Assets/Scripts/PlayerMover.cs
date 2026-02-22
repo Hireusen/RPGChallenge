2802e94e4777c673fd377a6556537e16d32d5987
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-02-22 13:34:11 +0900

    initial commit
    + 간단한 씬 작업

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ~ 오브젝트에 부착하는 C# 스크립트입니다.
+/// ~ 합니다.
+/// </summary>
+public class PlayerMover : MonoBehaviour
+{
+    #region ─────────────────────────▶ 인스펙터 ◀─────────────────────────
+    [Header("필수 요소 등록")]
+    [SerializeField] private Animator _animator;
+    [SerializeField] private CharacterController _controller;
+    [SerializeField] private Transform _camera;
+
+    [Header("사용자 정의 설정")]
+    [SerializeField] private float _walkSpeed = 5f;
+    [SerializeField] private float _runSpeed = 8f;
+    [SerializeField] private float _rotateSharpness = 15f;
+    #endregion
+
+    #region ─────────────────────────▶ 접근자 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 내부 변수 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 내부 메서드 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 외부 메서드 ◀─────────────────────────
+    // 인스펙터 유효성 검사
+    public void Verification() {
+
+    }
+
+    // 스크립트 내부 변수 초기화
+    public void Initialize() {
+
+    }
+
+    // 외부에 전달할 데이터 생성
+    public void DataBuilder() {
+
+    }
+    #endregion
+
+    #region ─────────────────────────▶ 메시지 함수 ◀─────────────────────────
+    private void Awake()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+
+    private void Reset()
+    {
+        
+    }
+
+    private void OnValidate()
+    {
+
+    }
+    #endregion
+}
