bc3f28e5eed0767eeb87adf120fbecd48b2abcf7
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-02-22 15:11:27 +0900

    feat: Constant, PlayerMover 작성 중

---
+++ Assets/Scripts/Utility/UMath.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+/// <summary>
+/// 수학 연산을 돕는 유틸리티 클래스입니다.
+/// </summary>
+public static class UMath
+{
+    /// <summary>
+    /// 원하는 높이에 도달하기 위한 초기 점프 속도를 계산합니다.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float CalcJumpVelocity(float targetHeight, float gravity)
+    {
+        return Mathf.Sqrt(targetHeight * -2f * gravity);
+    }
+}
